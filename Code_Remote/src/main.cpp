2b7e034fb01e0d544c2170a9a771e69e128b9f3e
Giojuve <giox362@gmail.com>
2026-04-27 22:47:39 +0200

    Update remote to byte based  format

--- Code_Remote/src/main.cpp
+++ Code_Remote/src/main.cpp
@@ -11,24 +11,25 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <constants.h>
 
-RF24 radio(7, 8); // CE, CSN
+#define THRESHOLD 10 //change if needed
+
+// Radio module setup
+RF24 radio(constantsPinsRemote::cePin, constantsPinsRemote::csnPin);
 
 const byte address[6] = "00001";
 
-//  Data structure for transmission
+// Buffer for sending data
+uint8_t buffer[constantsCom::bufferSize]; 
 
-struct Data {
-  int x;
-  bool motorOn;
-};
-
-Data data;
-
+// Pins
 const int joyX = A0;
 const int buttonPin = 2;
 
-
+// safe data for sending
+int lastX = 0;
+bool lastbutton = false;
 
 void setup()
 {
@@ -48,9 +49,47 @@ void setup()
 
 void loop()
 {
-  data.x = analogRead(joyX);                    // Read Joystick X-axis (0-1023)
-  data.motorOn = digitalRead(buttonPin) == LOW; // Read button -> true / false
+  // Clear buffer if needed
+  memset(buffer, 0, constantsCom::bufferSize); 
+  
+  int i = 0;
 
-  radio.write(&data, sizeof(data));             // send data    
+  // Keep Alive start byte
+  buffer[i++] = 1; 
+
+  // Steering
+  int x = analogRead(joyX);
+
+  // Send data when the change is significant
+  if (abs(x-lastX) > THRESHOLD){ 
+    uint8_t steer = map(x, 0, 1023,
+      constantsCom::steerMin, 
+      constantsCom::steerMax);
+      buffer[i++] = steer;
+      lastX = x;
+  }
+
+  // Motor button
+  bool button = !digitalRead(buttonPin);
+
+  if (button != lastbutton){
+    if (button){
+      buffer[i++] = constantsCom::throttleOn;
+    }
+    else{
+      buffer[i++] = constantsCom::throttleOff;
+    }
+    lastbutton = button;
+  }
+
+  // End of data
+  buffer[i] = 0; 
+
+  // Send
+  radio.write(buffer, constantsCom::bufferSize); 
+
   delay(20);
+
+
+  
 }
