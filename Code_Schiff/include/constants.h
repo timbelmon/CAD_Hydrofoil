f07d2e76e456c54c98e0a41d4c9392a504ffc9ca
kingpatxd <patrickvanallen536@gmail.com>
2026-04-27 13:21:54 +0200

    UNTESTED code for ship
    The code is theoreticlly complete.
    - new constants in constants.h
    - new functions for hardware communictaion
    Note: The code untested and probably need debuging.

--- Code_Schiff/include/constants.h
+++ Code_Schiff/include/constants.h
@@ -1,16 +1,35 @@
 #pragma once
 #include <stdint.h>
 
-namespace constants
+namespace constantsCom
 {
     const uint8_t bufferSize = 32;
     const uint8_t steerMin = 64;
     const uint8_t steerMax = 127;
     const bool throttleToggle = true;
-    const uint8_t throttleMin = 128;
-    const uint8_t throttleMax = 195;
+    const uint8_t throttleMin = 129;
+    const uint8_t throttleMax = 255;
     const uint8_t throttleOn = 2;
     const uint8_t throttleOff = 3;
-    const uint8_t foilStabOn = 4;
-    const uint8_t foilStabOff = 5;
+    const uint8_t throttleRev = 4;
+    const uint8_t foilStabOn = 5;
+    const uint8_t foilStabOff = 6;
+}
+
+namespace constantsPinsShip
+{
+    const uint8_t foilPin = 10;
+    const uint8_t servoSteerPin = 2;
+    const uint8_t csnPin = 8;
+    const uint8_t cePin = 7;
+    const uint8_t in1Pin = 3;
+    const uint8_t in2Pin = 5;
+    const uint8_t in3Pin = 6;
+    const uint8_t in4Pin = 9;
+}
+
+namespace constantsShip
+{
+    const uint8_t steerAngleMax = 0;
+    const uint8_t steerMidpoint = 90;
 }
--- Code_Schiff/src/main.cpp
+++ Code_Schiff/src/main.cpp
@@ -8,17 +8,22 @@
  */
 #include <Arduino.h>
 #include <constants.h>
+#include <Servo.h>
 #include <printf.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(constantsPinsShip::cePin, constantsPinsShip::csnPin); // CE, CSN
+Servo servo;
 
 const byte address[6] = "00001";
 
-bool getData(uint8_t *buffer, uint8_t *steer, uint8_t *throttle, uint8_t *foilStab);
 bool readRadio(uint8_t *buffer, RF24 *radio);
+bool getData(uint8_t *buffer, uint8_t *steer, uint8_t *throttle, bool *foilStab);
+void changeFoilStab(bool foilStab);
+void changeSteer(uint8_t steer);
+void changeThrottle(uint8_t throttle);
 
 void setup()
 {
@@ -35,20 +40,59 @@ void setup()
     radio.setPALevel(RF24_PA_MIN);
     radio.startListening();
     Serial.println("Receiver Initialized...");
+
+    pinMode(constantsPinsShip::foilPin, OUTPUT);
+    pinMode(constantsPinsShip::servoSteerPin, OUTPUT);
+    pinMode(constantsPinsShip::in1Pin, OUTPUT);
+    pinMode(constantsPinsShip::in2Pin, OUTPUT);
+    pinMode(constantsPinsShip::in3Pin, OUTPUT);
+    pinMode(constantsPinsShip::in4Pin, OUTPUT);
+    servo.attach(constantsPinsShip::servoSteerPin);
 }
 
 void loop()
 {
-    uint8_t *buffer = (uint8_t *)calloc(constants::bufferSize, sizeof(uint8_t));
+    static bool foilStab = false;
+    uint8_t steer = 0;
+    uint8_t throttle = 0;
+    static uint8_t buffer[constantsCom::bufferSize];
+
+    memset(buffer, 0, sizeof(buffer));
+    bool read = readRadio(buffer, &radio);
+    if (!read)
+    {
+        return;
+    }
+    bool tempFoilStab = foilStab;
+    bool keepAlive = getData(buffer, &steer, &throttle, &tempFoilStab);
+    if (tempFoilStab != foilStab)
+    {
+        foilStab = tempFoilStab;
+        changeFoilStab(foilStab);
+    }
+    if (steer != 0)
+    {
+        steer = constrain(steer, constantsCom::steerMin, constantsCom::steerMax);
+        steer = map(steer, constantsCom::steerMin, constantsCom::steerMax, 0, 255);
+        changeSteer(steer);
+    }
+    if (throttle != 0)
+    {
+        throttle = constrain(throttle, constantsCom::throttleMin, constantsCom::throttleMax);
+        throttle = map(throttle, constantsCom::throttleMin, constantsCom::throttleMax, 0, 255);
+        changeThrottle(throttle);
+    }
 }
 
 bool readRadio(uint8_t *buffer, RF24 *radio)
 {
     if (radio->available())
     {
-        radio->read(buffer, sizeof(buffer));
+        radio->read(buffer, constantsCom::bufferSize);
     }
     else
+        return false;
+    if (buffer[0] == 0)
         return false;
     return true;
 }
@@ -59,28 +103,69 @@ bool getData(uint8_t *buffer, uint8_t *steer, uint8_t *throttle, bool *foilStab)
     bool keepAlive = false;
     while (buffer[i] != 0)
     {
-        if (i = constants::bufferSize)
-            return;
-        if (buffer[i] = 1)
+        if (i == constantsCom::bufferSize)
+            break;
+        if (buffer[i] == 1)
         {
             keepAlive = true;
         }
-        else if (buffer[i] >= constants::steerMin && buffer[i] <= constants::steerMax)
+        else if (buffer[i] >= constantsCom::steerMin && buffer[i] <= constantsCom::steerMax)
             *steer = buffer[i];
-        else if (buffer[i] == constants::foilStabOn)
+        else if (buffer[i] == constantsCom::foilStabOn)
             *foilStab = true;
-        else if (buffer[i] == constants::foilStabOff)
+        else if (buffer[i] == constantsCom::foilStabOff)
             *foilStab = false;
-        else if (constants::throttleToggle)
+        else if (constantsCom::throttleToggle)
         {
-            if (buffer[i] == 2)
-                *throttle = constants::throttleMax;
-            if (buffer[i] == 3)
-                *throttle = constants::throttleMin;
+            if (buffer[i] == constantsCom::throttleOn)
+                *throttle = constantsCom::throttleMax;
+            if (buffer[i] == constantsCom::throttleOff)
+                *throttle = (constantsCom::throttleMin + constantsCom::throttleMax) / 2;
+            if (buffer[i] == constantsCom::throttleRev)
+                *throttle = constantsCom::throttleMin;
         }
-        else if (buffer[i] >= constants::throttleMin && buffer[i] <= constants::throttleMax)
+        else if (buffer[i] >= constantsCom::throttleMin && buffer[i] <= constantsCom::throttleMax)
             *throttle = buffer[i];
         i++;
     }
     return keepAlive;
+}
+
+void changeFoilStab(bool foilStab)
+{
+    digitalWrite(constantsPinsShip::foilPin, foilStab);
+}
+
+void changeSteer(uint8_t steer)
+{
+    steer = map(steer, 0, 255, (constantsShip::steerMidpoint - constantsShip::steerAngleMax), (constantsShip::steerMidpoint + constantsShip::steerAngleMax));
+    steer = constrain(steer, 0, 180);
+    servo.write(steer);
+}
+
+void changeThrottle(uint8_t throttle)
+{
+    if (throttle >= 126 && throttle <= 128)
+    {
+        analogWrite(constantsPinsShip::in1Pin, 0);
+        analogWrite(constantsPinsShip::in2Pin, 0);
+        analogWrite(constantsPinsShip::in3Pin, 0);
+        analogWrite(constantsPinsShip::in4Pin, 0);
+    }
+    else if (throttle > 128)
+    {
+        throttle = map(throttle, 127, 255, 0, 255);
+        analogWrite(constantsPinsShip::in1Pin, throttle);
+        analogWrite(constantsPinsShip::in2Pin, 0);
+        analogWrite(constantsPinsShip::in3Pin, throttle);
+        analogWrite(constantsPinsShip::in4Pin, 0);
+    }
+    else if (throttle < 126)
+    {
+        throttle = map(throttle, 125, 0, 0, 255);
+        analogWrite(constantsPinsShip::in1Pin, 0);
+        analogWrite(constantsPinsShip::in2Pin, throttle);
+        analogWrite(constantsPinsShip::in3Pin, 0);
+        analogWrite(constantsPinsShip::in4Pin, throttle);
+    }
 }
