b9fa76f6db6acd6f5eebc0cd13fb77aedc720a77
kingpatxd <patrickvanallen536@gmail.com>
2026-05-27 18:21:33 +0200

    Rebuild throttle logic

---
+++ Code_Remote/include/constants.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <stdint.h>
+
+namespace constantsCom
+{
+    const uint8_t bufferSize = 32;
+    const uint8_t steerMin = 64;
+    const uint8_t steerMax = 127;
+    const bool throttleToggle = false;
+    const uint8_t throttleMin = 129;
+    const uint8_t throttleMax = 255;
+    const uint8_t throttleOn = 2;
+    const uint8_t throttleOff = 3;
+    const uint8_t throttleRev = 4;
+    const uint8_t foilToggle = 5;
+}
+
+namespace constantsPinsShip
+{
+    const uint8_t foilPin = 10;
+    const uint8_t servoSteerPin = 2;
+    const uint8_t csnPin = 8;
+    const uint8_t cePin = 7;
+    const uint8_t in1Pin = 3;
+    const uint8_t in2Pin = 5;
+    const uint8_t in3Pin = 6;
+    const uint8_t in4Pin = 5;
+}
+
+namespace constantsShip
+{
+    const uint8_t steerAngleMax = 90;
+    const uint8_t steerMidpoint = 90;
+}
+
+namespace constantsPinsRemote
+{
+    const uint8_t cePin = 7;
+    const uint8_t csnPin = 8;
+    const uint8_t foilPin = 2;
+    const uint8_t speedPin = A0;
+    const uint8_t joyXPin = A1;
+}
--- Code_Remote/src/main.cpp
+++ Code_Remote/src/main.cpp
@@ -8,12 +8,13 @@
  */
 
 #include <Arduino.h>
+#include <Print.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <constants.h>
 
-#define THRESHOLD 10 //change if needed
+#define THRESHOLD 10 // change if needed
 
 // Radio module setup
 RF24 radio(constantsPinsRemote::cePin, constantsPinsRemote::csnPin);
@@ -21,75 +22,104 @@ RF24 radio(constantsPinsRemote::cePin, constantsPinsRemote::csnPin);
 const byte address[6] = "00001";
 
 // Buffer for sending data
-uint8_t buffer[constantsCom::bufferSize]; 
+uint8_t buffer[constantsCom::bufferSize];
 
 // Pins
-const int joyX = A0;
-const int buttonPin = 2;
+const int joyX = constantsPinsRemote::joyXPin;
+const int speedPin = constantsPinsRemote::speedPin;
+const int foilPin = constantsPinsRemote::foilPin;
 
 // safe data for sending
 int lastX = 0;
+int lastSpeed = 0;
 bool lastbutton = false;
 
 void setup()
 {
   Serial.begin(9600);
 
-  if (!radio.begin()){
+  if (!radio.begin())
+  {
     Serial.println("Radio error!");
-    while(1);
+    while (1)
+      ;
   }
 
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
+  radio.printDetails();
+  if (!radio.begin())
+  {
+    Serial.println("Hardware not responding! Check your wiring.");
+    radio.printDetails();
+    while (true)
+    {
+      /* code */
+    }
+  }
+  Serial.println("Remote Initialized...");
 
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(foilPin, INPUT_PULLUP);
 }
 
 void loop()
 {
   // Clear buffer if needed
-  memset(buffer, 0, constantsCom::bufferSize); 
-  
+  memset(buffer, 0, constantsCom::bufferSize);
+
   int i = 0;
 
   // Keep Alive start byte
-  buffer[i++] = 1; 
+  buffer[i++] = 1;
 
   // Steering
   int x = analogRead(joyX);
+  // Serial.println(x);
 
   // Send data when the change is significant
-  if (abs(x-lastX) > THRESHOLD){ 
-    uint8_t steer = map(x, 0, 1023,
-      constantsCom::steerMin, 
-      constantsCom::steerMax);
-      buffer[i++] = steer;
-      lastX = x;
+  if (abs(x - lastX) > THRESHOLD)
+  {
+    // TODO: THIS VALUE NEEDS CALEBRATION WHEN THE PCB IS READY
+    int maxReadSteer = 1023;
+    uint8_t steer = map(x, 0, maxReadSteer,
+                        constantsCom::steerMin,
+                        constantsCom::steerMax);
+    buffer[i++] = steer;
+    lastX = x;
+  }
+
+  // Speed
+  int speed = analogRead(speedPin);
+  // Serial.println(speed);
+  // Serial.println(lastSpeed);
+
+  if (abs(speed - lastSpeed) > THRESHOLD)
+  {
+    lastSpeed = speed;
+    // TODO: THIS VALUE NEEDS CALEBRATION WHEN THE PCB IS READY
+    int maxRead = 674;
+    speed = (int)map(speed, 0, maxRead, constantsCom::throttleMin, constantsCom::throttleMax);
+    buffer[i++] = speed;
   }
 
   // Motor button
-  bool button = !digitalRead(buttonPin);
+  bool button = !digitalRead(foilPin);
+  // Serial.println(button);
 
-  if (button != lastbutton){
-    if (button){
-      buffer[i++] = constantsCom::throttleOn;
-    }
-    else{
-      buffer[i++] = constantsCom::throttleOff;
-    }
-    lastbutton = button;
+  if (button != lastbutton && button)
+  {
+    buffer[i++] = constantsCom::foilToggle;
   }
 
-  // End of data
-  buffer[i] = 0; 
+  for (int j = 0; j < constantsCom::bufferSize; j++)
+  {
+    Serial.print(buffer[j]);
+  }
+  Serial.println("");
 
   // Send
-  radio.write(buffer, constantsCom::bufferSize); 
+  radio.write(buffer, constantsCom::bufferSize);
 
   delay(20);
-
-
-  
 }
